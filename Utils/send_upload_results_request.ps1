ff1b7e9218c2e4d5f3069037634a6d174d15990a
gpowala <gpowala@gmail.com>
2024-09-10 21:34:44 +0200

    Modify script to search for file in directory using defined pattern.

--- Utils/send_upload_results_request.ps1
+++ Utils/send_upload_results_request.ps1
@@ -1,7 +1,15 @@
 param(
     [Parameter(Mandatory=$true)]
-    [string]$xmlFilePath
+    [string]$xmlDirectory
 )
+
+$xmlFilePattern = "*Test*.xml"
+$xmlFilePath = Get-ChildItem -Path $xmlDirectory -Recurse -Filter $xmlFilePattern | Select-Object -First 1 -ExpandProperty FullName
+
+if (-not $xmlFilePath) {
+    Write-Error "No XML file matching the pattern '$xmlFilePattern' found in the specified directory or its subdirectories."
+    exit 1
+}
 
 $xmlContent = Get-Content -Path $xmlFilePath -Raw
 $escapedXmlContent = [System.Security.SecurityElement]::Escape($xmlContent)
