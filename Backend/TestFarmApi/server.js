bcdb94c7b4970fac0e17063057a55f4331b9fb04
gpowala <gpowala@gmail.com>
2024-09-19 00:38:45 +0200

    Increase maximum request size to 100mb to allow big results uploads.

--- Backend/TestFarmApi/server.js
+++ Backend/TestFarmApi/server.js
@@ -16,6 +16,9 @@ const port = 3000;
 app.use(express.json());
 app.use(cors());
 
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ limit: '100mb', extended: true }));
+
 const swaggerOptions = {
   definition: {
     openapi: '3.0.0',
