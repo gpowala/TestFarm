45fec9405b9575e8117332c4237b389a89a7f532
gpowala <gpowala@gmail.com>
2025-10-15 23:10:57 +0200

    Fixes database migration configuration.

--- Backend/TestFarmApi/config/config.js
+++ Backend/TestFarmApi/config/config.js
@@ -2,18 +2,75 @@ const { appSettings } = require('../appsettings');
 
 module.exports = {
   development: {
-    dialect: 'sqlite',
-    storage: appSettings.database.sqliteDatabasePath,
+    dialect: 'mssql',
+    host: appSettings.database.host,
+    database: appSettings.database.database,
+    username: appSettings.database.username,
+    password: appSettings.database.password,
+    dialectOptions: {
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+        enableArithAbort: true,
+        connectionTimeout: 60000,
+        requestTimeout: 60000
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000,
+      idle: 10000
+    },
+    logging: console.log,
     migrationStorageTableName: 'sequelize_meta'
   },
   test: {
-    dialect: 'sqlite',
-    storage: appSettings.database.sqliteDatabasePath,
+    dialect: 'mssql',
+    host: appSettings.database.host,
+    database: appSettings.database.database,
+    username: appSettings.database.username,
+    password: appSettings.database.password,
+    dialectOptions: {
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+        enableArithAbort: true,
+        connectionTimeout: 60000,
+        requestTimeout: 60000
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000,
+      idle: 10000
+    },
+    logging: console.log,
     migrationStorageTableName: 'sequelize_meta'
   },
   production: {
-    dialect: 'sqlite',
-    storage: appSettings.database.sqliteDatabasePath,
+    dialect: 'mssql',
+    host: appSettings.database.host,
+    database: appSettings.database.database,
+    username: appSettings.database.username,
+    password: appSettings.database.password,
+    dialectOptions: {
+      options: {
+        encrypt: false,
+        trustServerCertificate: true,
+        enableArithAbort: true,
+        connectionTimeout: 60000,
+        requestTimeout: 60000
+      }
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 60000,
+      idle: 10000
+    },
+    logging: false,
     migrationStorageTableName: 'sequelize_meta'
   }
 };
